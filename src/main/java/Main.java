cfb05dea92d54b982c9dfe6fd12a691d5e08f3b4
Carlos <carlos@carloslandia.com>
2022-03-22 09:53:22 +0100

    Preparando el Push

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Mi primer programa en Intellij");
+        System.out.println("Proyecto listo para GitHub");
 
     }
 }
