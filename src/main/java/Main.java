f77ae545f85f59e6b48d54d3d6dfd9d53a656464
Carlos <carlos@carloslandia.com>
2022-03-22 10:30:35 +0100

    Adivinar Palabra

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Proyecto listo para GitHub");
+
+        String pass = "1234";
+        Scanner sc = new Scanner(System.in);
+        String texto;
+        do {
+            System.out.println("Adivina mi contraseña secreta");
+            texto = sc.nextLine();
+            System.out.println("He recibido " + texto);
+        } while (!texto.equals(pass));
+
 
     }
+
+
 }
